2d696d2e600f773dd63380575f46669e7a864686
Stuart Bradley <stuy.bradley@gmail.com>
2024-03-25 21:44:52 +0000

    Partial solution.

--- take_home_test_starter/__main__.py
+++ take_home_test_starter/__main__.py
@@ -1,7 +1,27 @@
-def main() -> None:
-    """The main method of the application"""
-    print("Hello, world.")
+import argparse
+import os
 
+import fiona
+import rasterio
+import rasterio.mask
+
+
+def main() -> None:
+    """
+    This script performs the following tasks:
+    1. Opens a Shapefile via Fiona (GDAL)
+    2. Downloads related GeoTIFF Files (Boto3 or direct download).
+    3. Merge related GeoTIFF files (GDAL Merge).
+    4. Uses Rasterio to create a mask with only the area of interest.
+      (https://rasterio.readthedocs.io/en/stable/topics/masking-by-shapefile.html)
+    """
+
+    #parser = argparse.ArgumentParser(description='Generates GeoTIFF files from Shapefiles')
+    #parser.add_argument('filename')
+    #parsed_args = parser.parse_args()
+
+    with fiona.open("/app/take_home_test_starter/world-administrative-boundaries.shp", "r") as shapefile:
+        shapes = [feature["geometry"] for feature in shapefile]
 
 if __name__ == "__main__":
     main()
